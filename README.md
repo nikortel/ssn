cbf160c50d078115f3c58ffdf5c6c040dae9fe02
Niko Kortelainen <nikortel@gmail.com>
2017-05-07 21:55:57 +0100

    Adds examples

--- README.md
+++ README.md
@@ -5,7 +5,21 @@ A Clojure library designed to social security number validation via spec.
 ## Usage
 
 ```clojure
+(:require [ssn.finnish :as ssn]
+          [clojure.spec.alpha :as s]
+          [clojure.spec.gen.alpha :as gen])
+
+;;Validate social security number
 (s/valid? :ssn.finnish/social-security-number "040597-9753")
+
+;;Generate a social security number for specific birthdate and gender
+(ssn/generate-social-security-number {:ssn.finnish/day 1
+                                      :ssn.finnish/month 1
+                                      :ssn.finnish/year 1986
+                                      :ssn.finnish/gender :female})
+
+;;Generate a random social security number
+(gen/generate (s/gen :ssn.finnish/social-security-number))
 ```
 
 ## License
--- src/ssn/core.clj
+++ src/ssn/core.clj
@@ -1,6 +1,14 @@
 (ns ssn.core
-  (:require [ssn.finnish :as ssn])
+  (:require [ssn.finnish :as ssn]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen])
   (:gen-class))
 
 (defn -main [& args]
-  (println "SSN:" (ssn/generate-random-social-security-number)))
+  (println "010186-566W valid?" (s/valid? :ssn.finnish/social-security-number "010186-566W"))
+  (println "SSN:" (gen/generate (s/gen :ssn.finnish/social-security-number)))
+  (println "SSN:" (ssn/generate-social-security-number {:ssn.finnish/day 1
+                                                        :ssn.finnish/month 1
+                                                        :ssn.finnish/year 1986
+                                                        :ssn.finnish/gender :female}))
+  )
