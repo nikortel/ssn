766e1677dff4255d576b9b57fb99984533345a77
Niko Kortelainen <nikortel@gmail.com>
2017-05-31 21:03:36 +0100

    Updates Clojure and ClojureScript versions

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/clojurescript "1.9.542"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.562"]]
 
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
 
